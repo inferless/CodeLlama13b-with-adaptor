d159dd549e60fe33f119848a4b22274e2ca55789
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-05 16:39:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"]='1'
+from huggingface_hub import snapshot_download
 from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 
-
 class InferlessPythonModel:
     def initialize(self):
+        model_id = "codellama/CodeLlama-13b-Instruct-hf"
+        snapshot_download(repo_id=model_id,allow_patterns=["*.safetensors"])
         config = PeftConfig.from_pretrained("G-ML-Hyly/stg-cli13b-t6-cdp-ca.mt.him.cln.inter-b4s1e1-20231219-1611")
-        self.model = AutoModelForCausalLM.from_pretrained("codellama/CodeLlama-13b-Instruct-hf")
+        self.model = AutoModelForCausalLM.from_pretrained(model_id)
         self.model = PeftModel.from_pretrained(self.model, "G-ML-Hyly/stg-cli13b-t6-cdp-ca.mt.him.cln.inter-b4s1e1-20231219-1611")
         self.model.to("cuda:0")
         self.tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-13b-Instruct-hf")
